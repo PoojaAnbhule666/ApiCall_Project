5debc9c52e44469d4320a01ea11897afd613af3a
Pooja <anbhulepooja250@gmail.com>
2020-01-31 15:08:03 +0530

    create the employe list Vc & Employee detail Vc

---
+++ Employee_Details/Employee_Details/ApiCallManager.swift
@@ -0,0 +1,149 @@
+//
+//  ApiCallManager.swift
+//  Config
+//
+//  Created by Deepak Yadav on 11/07/19.
+//  Copyright © 2019 wits. All rights reserved.
+//
+
+import Foundation
+
+class Apicall : NSObject {
+    
+    private var sharedSession: URLSession {
+        let config = URLSessionConfiguration.default
+        config.timeoutIntervalForRequest = 60
+        config.requestCachePolicy = .reloadIgnoringLocalAndRemoteCacheData
+        config.httpAdditionalHeaders = ["Cache-Control" : "no-cache"]
+        return URLSession(configuration: config)
+    }
+    
+    static let sharedInstance = Apicall()
+    
+    
+   
+    
+    // MARK:- services methods
+    
+    class func getStringFromData(data: Data) -> String? {
+        guard let str = String(data: data, encoding: .utf8) else { return nil }
+        return str
+    }
+    class func getJSONObject(data: Data) -> Any? {
+        do {
+            let jsonObject = try JSONSerialization.jsonObject(with: data,
+                                                              options: [])
+            return jsonObject
+        } catch {
+            return nil
+        }
+    }
+    
+    class func getJSONStringFromObject(_ jsonObject: Any) -> String? {
+        do {
+            let jsonData = try JSONSerialization.data(withJSONObject: jsonObject,
+                                                      options: .init(rawValue: 0))
+            return Apicall.getStringFromData(data: jsonData)
+        } catch {
+            return nil
+        }
+    }
+    
+    
+    
+    func checkConfig(withParameters parameters:Any, completionBlock : @escaping (_ successful:Bool,_ response:Any?) -> ()) {
+        sendPOSTDataWithoutDataKey(serviceUrl: "http://10.100.0.25:9090/bnb_nms/ar_app/asset_bundles/get_bundle.php", parameters: parameters, completionBlock: completionBlock)
+    }
+
+    
+    func checkFutureDAte(completionBlock : @escaping (_ successful:Bool,_ responseDict:Any?) -> ()) {
+        let urlString = "http://10.100.0.25:9090/bnb_nms/ar_app/log_fdate.php"
+        callGETservice(serviceURL: urlString, completionBlock: completionBlock)
+    }
+
+    
+    func sendPOSTDataWithoutDataKey(serviceUrl : String, parameters:Any, completionBlock : @escaping (_ successful:Bool,_ response:Any?) -> ()) {
+        guard let url = URL(string: "\(serviceUrl)")
+            else {
+                DispatchQueue.main.async
+                    {
+                        completionBlock(false, "Invalid URL")
+                }
+                return
+        }
+        
+        do {
+            let data = try JSONSerialization.data(withJSONObject: parameters,
+                                                  options: [])
+            var request = URLRequest(url: url)
+            request.httpMethod = "POST"
+            request.httpBody = data
+            request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+            request.addValue("application/json", forHTTPHeaderField: "Accept")
+            
+            let dataTask = sharedSession.dataTask(with: request) { (data, response, error) in
+                if error == nil {
+                    DispatchQueue.main.async {
+                        
+                        print("\(#function) :  Success \n \(Apicall.getStringFromData(data: data!) ?? "")")
+//                        print("\(#function) : Success \n \(String(describing: Apicall.getStringFromData(data: data!)))")
+//                        completionBlock(true,(Apicall.getJSONObject(data: data!)))
+                        completionBlock(true,(Apicall.getStringFromData(data: data!)))
+                    }
+                } else {
+                    print("\(#function) error : \(error?.localizedDescription ?? "No error")")
+                    
+                    DispatchQueue.main.async {
+                        completionBlock(false, "Connection Error")
+                    }
+                }
+            }
+            dataTask.resume()
+        } catch let error {
+            print("\(#function) error : \(error.localizedDescription)")
+            DispatchQueue.main.async {
+                completionBlock(false, "Connection Error")
+            }
+        }
+    }
+
+    
+    func callGETservice(serviceURL : String, completionBlock : @escaping (_ successful:Bool,_ responseDict:Any?) -> ()) {
+        guard let url = URL(string: "\(serviceURL)") else { return }
+        print("url is-->> \(url)")
+        let dataTask = sharedSession.dataTask(with: url,
+                                              completionHandler: { (data, response, error) in
+                                                
+                                                
+                                                if error == nil {
+                                                    print("completion block done")
+                                                    //                                                    if AppCommonData.getJSONObject(data: data!) != nil
+                                                    print("\(String(describing: response))")
+                                                    
+                                                    
+                                                    print(" \(#function) : Success \n \(String(describing: Apicall.getStringFromData(data: data!)))")
+                                                    
+                                                    //                                                        print("\(#function) : Success \n \(AppCommonData.getJSONObject(data: data!)!)")
+                                                    DispatchQueue.main.async {
+                                                        //                                                        completionBlock(true,(Apicall.getJSONObject(data: data!)))
+                                                        completionBlock(true,(Apicall.getStringFromData(data: data!)))
+                                                    }
+                                                    //                                                    }else
+                                                    //                                                    {
+                                                    //                                                        completionBlock(false,"Somethig went wrong")
+                                                    //                                                    }
+                                                    
+                                                } else {
+                                                    
+                                                    print("\(#function) : failed (\(error?.localizedDescription ?? "No error"))")
+                                                    DispatchQueue.main.async {
+                                                        
+                                                        completionBlock(false,(error?.localizedDescription ?? "No error"))
+                                                    }
+                                                }
+        })
+        dataTask.resume()
+    }
+    
+    
+}
---
+++ Employee_Details/Employee_Details/EmployeeDetailsViewController.swift
@@ -0,0 +1,34 @@
+//
+//  EmployeeDetailsViewController.swift
+//  Employee_Details
+//
+//  Created by Pooja Anbhule on 30/01/20.
+//  Copyright © 2020 nttdata. All rights reserved.
+//
+
+import UIKit
+
+class EmployeeDetailsViewController: UIViewController {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var txtEmployeeName: UITextField!
+    @IBOutlet weak var txtEmployeeAge: UITextField!
+    @IBOutlet weak var txtEmployeeSalary: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
